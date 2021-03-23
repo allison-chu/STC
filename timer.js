5bd4adf218d6b443b9f21630b584cc399dabc62e
“Allison <im.allison.chu@gmail.com>
2021-03-23 16:17:24 -0700

    added assets/updated reg

--- timer.js
+++ timer.js
@@ -42,5 +42,5 @@ function initializeClock(id, endtime) {
   var timeinterval = setInterval(updateClock, 1000);
 }
 
-var deadline = 'May 03 2021 8:00:00';
+var deadline = 'May 08 2021 8:00:00';
 initializeClock('clockdiv', deadline);
